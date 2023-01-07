016bea81c6c4a72b426b6c501a2875cdcf2f3594
Sangram <sangramkesharichoudhury6@gmail.com>
2023-01-07 15:43:47 +0530

    CBVproject

---
+++ CBVproject/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from CBVapp.views import CourseListView,CourseDetailView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/',include('CBVapp.urls')),
+]
